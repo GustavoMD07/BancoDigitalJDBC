fa285f641073b40d6c38b8e018bd8c16d654ac10
Gustavo <gustavomatachun.domingues@gmail.com>
2025-05-09 16:58:43 -0300

    teste vscode

--- src/main/java/br/com/cdb/bancodigitalJPA/BancodigitalJpaApplication.java
+++ src/main/java/br/com/cdb/bancodigitalJPA/BancodigitalJpaApplication.java
@@ -3,8 +3,6 @@ package br.com.cdb.bancodigitalJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//quando usamos JPA, preciso ir no application.properties, para "configurações"
-
 @SpringBootApplication
 public class BancodigitalJpaApplication {
 
